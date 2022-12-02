bf8e1a46d9aab8de1f842327d7c0924caf635a5d
Juanma Romalde <juanma0089@hotmail.com>
2022-12-02 18:48:37 +0100

    terminado el ejercicio 4 creado el repo nuevamente

--- 004searchElements/004searchElements.js
+++ 004searchElements/004searchElements.js
@@ -1,7 +1,7 @@
 // 004searchElements.html/.js:Aquí está el documento con la tabla y el formulario.
 //¿Cómo encontrar?...
 //La tabla con id="age-table".
-//T.odos los elementos labeldentro de la tabla (debería haber 3).
+//T.odos los elementos label dentro de la tabla (debería haber 3).
 //El primer td en la tabla (con la palabra “Age”).
 //El form con name="search"
 //El primer input en ese formulario.
@@ -10,6 +10,35 @@
 //navegador..
 'use strict';
 
+
+//?La tabla con id="age-table".
 let tabla = document.getElementById('age-table');
 
 console.log(tabla);
+
+//?T.odos los elementos label dentro de la tabla (debería haber 3).
+let elementosTabla = tabla.querySelectorAll('td > label');
+
+console.log(elementosTabla);
+
+
+//?El primer td en la tabla (con la palabra “Age”).
+let primerElement = tabla.getElementsByTagName('td')[0];
+
+console.log(primerElement);
+
+
+//?El form con name="search"
+let formName = document.getElementsByName('search')[0];
+
+console.log(formName);
+
+//?El primer input en ese formulario.
+let primerInput = formName.querySelector('input');
+
+console.log(primerInput);
+
+//?El último input en ese formulario.
+let ultimoInput = formName.querySelectorAll('input');
+
+console.log(ultimoInput[ultimoInput.length-1]);
