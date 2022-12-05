aa6a752c0700da64a512af09ef5cafbbb0c8157f
juanma0089 <juanma0089@hotmail.com>
2022-12-05 11:19:35 +0100

    ej 11

--- 007tagInComment/007tagInComment.js
+++ 007tagInComment/007tagInComment.js
@@ -1,1 +1,13 @@
 // 007tagInComment.html/.js: Etiqueta en comentario: ¿Qué muestra este código?
+
+"use strict";
+let body = document.body;
+        body.innerHTML = "<!--" + body.tagName + "-->";
+        alert( body.firstChild.data );
+
+        
+        // El resultado del alert es BODY.
+        // Se está reemplazando el contenido del body con el comentario ("<!--" + body.tagName + "-->
+        // Al usar tagName siempre va en mayúsculas. de ahí el alert en mayúsculas.
+        // Con firstChild obtenemos el primer nodo hijo, en este caso solo hay uno que es el comentario.
+        // Con data obtenemos el contenido del comentario. 
---
+++ 011cleaning/011cleaning.js
@@ -0,0 +1,13 @@
+// 011cleaning.html/.js: Crea una función clear(elem) que remueva todo del elemento. 
+"use strict";
+
+let elem =  document.getElementById('elem');
+
+clear(elem);
+
+function clear(elem){
+
+    elem.remove();
+
+}
+
