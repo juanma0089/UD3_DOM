9dd4830471ce3463e388e2e85069408ee4a2431e
Juanma Romalde <juanma0089@hotmail.com>
2022-12-19 20:45:28 +0100

    ejercicio 28 terminado

--- 028imageGallery/028imageGallery.js
+++ 028imageGallery/028imageGallery.js
@@ -1,3 +1,15 @@
 // 028imageGallery.html/.js:Crea una galería de imágenes donde la imagen principal 
 // cambia al hacer clic en una miniatura. Como en Amazon, por ejemplo.
 'use strict';
+
+function galeria(element, src)
+{
+    element = seleccion(element);
+    element.src = src;
+}
+
+function seleccion(element)
+{
+    return document.getElementById(element);
+}
+
