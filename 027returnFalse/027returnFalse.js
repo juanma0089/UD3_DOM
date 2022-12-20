7f12861279c75ae10ad41362ffbd769a80c19677
juanma0089 <juanma0089@hotmail.com>
2022-12-20 11:54:15 +0100

    27 terminado

--- 027returnFalse/027returnFalse.js
+++ 027returnFalse/027returnFalse.js
@@ -7,7 +7,8 @@
 // </script>
 // <a href="https://w3.org" onclick="handler()">el navegador irá a w3.org</a>
 'use strict';
-
+//*La sintaxis no es correcta, un alert no debería estar dentro de una función. 
+//*Nunca llega al return, rompe el árbol de eventos.
 function handler() {
     alert( "..." );
     return false;
